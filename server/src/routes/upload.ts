d41256882599ae5474e7c9489f14476e4e69dc81
Jason Everton <jasonemsw10@gmail.com>
2023-05-25 20:50:32 -0300

    Corrections2

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -37,8 +37,7 @@ export async function uploadRoutes(app: FastifyInstance) {
     await pump(upload.file, writeStream)
 
     const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`../uploads/${fileName}`, fullUrl).toString()
-    console.log(fileUrl)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
 
     return { fileUrl }
   })
